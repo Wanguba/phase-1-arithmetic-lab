39eee33f97105154b9af23c13bdd4721cca73703
Edith Wanguba <wanguamor@gmail.com>
2023-09-19 09:02:30 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2 ;
+console.log(multiply)
+
+const random = 50
+if (random > 0 && Number.isInteger(random)){
+
+    console.log ('greater than 0')
+}
+const num3 = 14;
+const num4 = 10;
+
+const mod = ( num3 % num4);
+console.log (mod);
+
+const numbers = [ 4, 5, 10, 13, 16,20];
+const max = Math.max(...numbers);
